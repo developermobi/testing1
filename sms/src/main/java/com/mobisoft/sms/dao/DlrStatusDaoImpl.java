c2b50d6388c6cf7bb32a9289b984e3e2c414f5fd
sani07mobi <pramod.v@mobisofttech.co.in>
2017-08-18 20:29:48 +0530

    add procedure

--- sms/src/main/java/com/mobisoft/sms/dao/DlrStatusDaoImpl.java
+++ sms/src/main/java/com/mobisoft/sms/dao/DlrStatusDaoImpl.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.Query;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -57,10 +59,36 @@ public class DlrStatusDaoImpl implements DlrStatusDao{
 			while((line = br.readLine()) != null)
 			{
 				mobileList.add(line);
+				
 			}
-			System.out.println(mobileList.size());
-		} catch (Exception e) {
+			for (int i = 0; i < mobileList.size(); i++) {
+				org.hibernate.Query query = session.createSQLQuery("CALL spInsert(:job_id,:Sender,:momt,:type,:mclass,:coding,:message,:mobile,:provider_id,:user_id,:account)")
+						  .addEntity(DlrStatus.class)
+						  .setParameter("job_id",list.get(0).getId())
+						  .setParameter("Sender",list.get(0).getSender())
+						  .setParameter("momt", "MO")
+						  .setParameter("type", 1)
+						  .setParameter("mclass", 1)
+						  .setParameter("coding", 0)
+
+						  .setParameter("message",list.get(0).getMessage())
+
+						  .setParameter("mobile",mobileList.get(0))
+						  .setParameter("provider_id","New Foo")
+
+						  .setParameter("user_id",Integer.toString(list.get(0).getUserId()))
+						  .setParameter("account",Integer.toString(list.get(0).getUserId()));
+				query.executeUpdate();
+				if( i % 50 == 0 ) { // Same as the JDBC batch size
+					
+			        session.flush();
+			        session.clear();
+				}
+			}
 			
+			System.out.println("aFTER pROCEDURES"+mobileList.size());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}finally{
 			
 		}
